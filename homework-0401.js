e9fa9b8d0bb09943b8fd34b55fc4473a38c727c7
babubu157 <gboloro@gmail.com>
2025-04-01 23:53:18 +0800

    0401 async await homework

---
+++ homework-0401.js
@@ -0,0 +1,52 @@
+const fs = require("fs");
+/////////////////////// Bodlogo 1 //////////////////////
+
+// const getDatafromJSON = async () => {
+//   const dataFromJSON = await fs.readFileSync("./insta.json", "utf-8");
+//   //console.log(dataFromJSON);
+//   const parsedData = JSON.parse(dataFromJSON);
+//   //console.log(parsedData);
+//   return parsedData;
+// };
+
+// const printSuggestedUsers = async () => {
+//   const users = await getDatafromJSON();
+//   const suggestedUsers = users.forEach((el) => {
+//     if (el.suggested) console.log(el.username);
+//   });
+// };
+// printSuggestedUsers();
+
+///////////////////// Bodlogo 2 ////////////////////////
+let myID = "";
+const getDatafromJSON = async () => {
+  const dataFromJSONfile = await fs.readFileSync("./insta.json", "utf-8");
+  const parsedData = JSON.parse(dataFromJSONfile);
+  return parsedData;
+};
+
+const findMyID = async () => {
+  const getAllUsers = await getDatafromJSON();
+  getAllUsers.forEach((el) => {
+    if (el.nickname === "25LP0000") {
+      myID = el.nickname;
+    }
+    return myID;
+  });
+};
+
+const findMyFollows = async () => {
+  const myid1 = await findMyID();
+  const getAllUsers1 = await getDatafromJSON();
+  const result = getAllUsers1.map((user) => {
+    //   console.log(myID);
+
+    if (user.followedByNickName === myid1) {
+      console.log(user);
+      return user;
+    }
+  });
+  console.log(result);
+};
+
+findMyFollows();
